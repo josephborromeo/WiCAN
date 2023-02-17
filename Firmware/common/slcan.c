7705ad576cead84361759cffb74fcee460fff2e5
Joseph Borromeo <joseph.borromeo00@gmail.com>
2023-02-17 14:04:23 -0500

    starting SLCAN formatting

--- Firmware/common/slcan.c
+++ Firmware/common/slcan.c
@@ -1,20 +1,16 @@
 #include "slcan.h"
 
+#include "driver/twai.h"
+
+// Convert Incoming CAN message into SLCAN formatted "string"
 const char* slcan_format(twai_message_t message){
-    char output_string[19]; // Max Length of SLCAN Message
-    // Start of Frame
-    strcat(output_string, SOF);
-
-    // Timestamp
-
-    // DLC
-
-    // Arbitration ID
-
-    // Payload (Data)
-
-    // End of Frame
-    strcat(output_string, EOF);
+    uint8_t msg_buffer[SLCAN_MTU]; // Max Length of SLCAN Message
+    
+    // Fill Buffer with null chars
+    for (uint8_t j=0; j < SLCAN_MTU; j++)
+    {
+        msg_buffer[j] = '\0';
+    }
 
     return output_string;
 }
--- Firmware/common/slcan.h
+++ Firmware/common/slcan.h
@@ -1,3 +1,8 @@
+// maximum rx buffer len: extended CAN frame with timestamp 
+#define SLCAN_MTU 30 // (sizeof("T1111222281122334455667788EA5F\r")+1)
 
-#define SOF 0xAA
-#define EOF 0xBB
+#define SLCAN_STD_ID_LEN 3
+#define SLCAN_EXT_ID_LEN 8
+
+
+const char* slcan_format(twai_message_t message);
